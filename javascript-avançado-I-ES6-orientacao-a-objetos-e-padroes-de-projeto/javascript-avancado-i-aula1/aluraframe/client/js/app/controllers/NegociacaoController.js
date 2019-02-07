94a51c6df3bc36b3fa3ebacf67977bcd504f6737
Rian Perassoli <rian.perassoli@gmail.com>
2019-02-07 20:46:40 -0200

    Aula 3
    Criado controller de3 negociação e iniciado o uso de map, split, e arrow functions

---
+++ javascript-avançado-I-ES6-orientacao-a-objetos-e-padroes-de-projeto/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,30 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        //console.log(typeof(this._inputData.value))
+
+        let data = new Date(
+            ...this._inputData.value
+                .split('-').map((item, indice) => item - indice % 2)
+                //poderia ser .map(function(item, indice) {return item - indice % 2})
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+}
